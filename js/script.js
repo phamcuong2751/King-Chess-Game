c16b6772c56995cae764f92bed19b2fdedd2877e
phamcuong2751 <phamcuong2751@gmail.com>
2020-08-23 10:59:06 +0700

    add folder

--- js/script.js
+++ js/script.js
@@ -12,19 +12,8 @@ for (var i = 0; i < 8; i++) {
             if (j % 2 == 0) content += '<div class="block-black" onclick="indexFunction(this)" id="' + indexAlphabet[i] + j + '"></div>';
             else content += '<div class="block-white" onclick="indexFunction(this)" id="' + indexAlphabet[i] + j + '"></div>';
         }
-        // if (i < 2) {
-        //     content = content + chessmansBlack[(i * 8) + j] + '</div>';
-        // } else if (i >= 6) {
-        //     content = content + chessmansWhite[(i - 6) * 8 + j] + '</div>';
-        // } else {
-        //     content += '</div>';
-        // }
     }
 }
 chessboard.innerHTML = content;    
 
-function indexFunction(idElement)
-{
-    var fakeID = idElement.id;
-    console.log(this.fakeID)
-}
+function indexFunction(idElement){ return idElement.id}
